48788a91a6ba675659263edcff5d6128f2565205
Sirisha-143 <kokkirigaddasirisha@gmail.com>
2026-04-24 00:53:36 +0530

    Create funwithparam.py

---
+++ funwithparam.py
@@ -0,0 +1,9 @@
+def display_info(name, age):
+    print(f'name:{name}')
+    print(f'age:{age}')
+def other_info(designation, company, salary):
+    print(f'designation:{designation}')
+    print(f'company:{company}')
+    print(f'salary:{salary}')
+display_info("Bhavana", 25)
+other_info("Developer", "TCS", 20000)
